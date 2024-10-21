909403f9fcff2d96d86984377a4de75a41bc7223
densavi <d.savitsky93@gmail.com>
2024-10-21 20:42:27 +0300

    sidebar

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,16 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
+import $ from 'jquery';
+
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	$('.has-submenu').each(function() {
+		$(this).find('> a').on('click', function(e) {
+			e.preventDefault();
+			$(this).parent().toggleClass('active');
+			$(this).parent().find('.submenu').slideToggle(500);
+		})
+	})
 
 })
